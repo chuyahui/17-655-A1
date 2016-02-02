13867ec14c040cf2da0568074b213bbcdd96bd44
Weinan Qiu <davidiamyou@gmail.com>
2016-02-01 21:39:50 -0500

    file source filter comment.

--- A1/common/src/main/java/shared/FileSourceFilter.java
+++ A1/common/src/main/java/shared/FileSourceFilter.java
@@ -8,19 +8,36 @@ import java.io.IOException;
 import java.io.InputStream;
 
 /**
- * @author Weinan Qiu
+ * A source filter that reads from a file. The file must be specified as an absolute path.
+ *
  * @since 1.0.0
  */
 public class FileSourceFilter extends SourceFilterTemplate {
 
+    /**
+     * The input stream corresponding to the file to read data from.
+     */
     private InputStream in;
 
+    /**
+     * Default constructor. It will attempt to open the file input stream for the file specified.
+     *
+     * @param filterId id for this filter
+     * @param fileName the absolute file path for the file to be read
+     *
+     * @throws FileNotFoundException thrown when file cannot be found.
+     */
     public FileSourceFilter(final String filterId, final String fileName) throws FileNotFoundException {
         super(filterId);
         in = new FileInputStream(fileName);
-        //in = getClass().getResourceAsStream(fileName);
     }
 
+    /**
+     * read a byte of data from the file. throw an {@link RuntimeException} if {@link IOException} occurs
+     * while reading the file.
+     *
+     * @return byte data read from file.
+     */
     @Override
     protected byte readOneByte() {
         try {
@@ -30,6 +47,11 @@ public class FileSourceFilter extends SourceFilterTemplate {
         }
     }
 
+    /**
+     * Whether the file input stream has reached to an end.
+     *
+     * @return true if there's no more data in the file, false if there's still data to be read.
+     */
     @Override
     protected boolean hasReachedEndOfStream() {
         try {
