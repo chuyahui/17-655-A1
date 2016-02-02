108e766c00914c736aebc85887bef311b12a9400
Weinan Qiu <davidiamyou@gmail.com>
2016-02-01 20:52:24 -0500

    source filter comment.

--- A1/common/src/main/java/framework/SourceFilterTemplate.java
+++ A1/common/src/main/java/framework/SourceFilterTemplate.java
@@ -2,34 +2,10 @@ package framework;
 
 import java.io.EOFException;
 
-/******************************************************************************************************************
-* File:SourceFilterTemplate.java
-* Course: 17655
-* Project: Assignment 1
-* Copyright: Copyright (c) 2003 Carnegie Mellon University
-* Versions:
-*	1.0 November 2008 - Initial rewrite of original assignment 1 (ajl).
-*
-* Description:
-*
-* This class serves as a template for creating source filters. The details of threading, connections writing output
-* are contained in the FilterFramework super class. In order to use this template the program should rename the class.
-* The template includes the run() method which is executed when the filter is started. The run() method is the guts
-* of the filter and is where the programmer should put their filter specific code.The run() method is the main
-* read-write loop for reading data from some source and writing to the output port of the filter. This template
-* assumes that the filter is a source filter that reads data from a file, device (sensor),or generates the data
-* interally, and then writes data to its output port. In this case, only the output port is used. In cases where the
-* filter is a standard filter or a sink filter, you should use the SimpleFilter.java or SinkFilterTemplate.java as
-* a starting point for creating standard or sink filters.
-*
-* Parameters: 		None
-*
-* Internal Methods:
-*
-*	public void run() - this method must be overridden by this class.
-*
-******************************************************************************************************************/
-
+/**
+ * The class represents the source, a special kind of filter that reads data from an external resource and pass it on.
+ * It provides basic functionality of pass the data to the output port
+ */
 public abstract class SourceFilterTemplate extends FilterFramework
 {
 
@@ -45,7 +21,7 @@ public abstract class SourceFilterTemplate extends FilterFramework
 	}
 
 	/**
-	 * Template method to read one byte of data from the input stream
+	 * Template method to read one byte of data from the external resource
 	 *
 	 * @return the byte read from the input stream
 	 */
@@ -58,16 +34,13 @@ public abstract class SourceFilterTemplate extends FilterFramework
 	 */
 	protected abstract boolean hasReachedEndOfStream();
 
-	public void run()
-    {
+	/**
+	 * Main execution method for the source filter. It reads a byte of data and pass it onto output port.
+	 * If the external resource has ended, it throws {@link EOFException}
+	 */
+	public void run() {
 
 		byte databyte = 0;
-
-/*************************************************************
-*	This is the main processing loop for the filter. Since this
-*   is a source filter, the programer will have to determine
-* 	when the loop ends.
-**************************************************************/
 
 		try {
 			while (true) {
@@ -83,6 +56,5 @@ public abstract class SourceFilterTemplate extends FilterFramework
 		} catch (Exception ex) {
 			System.out.println("Encountered exception: " + ex.getMessage());
 		}
-   } // run
-
-} // SourceFilterTemplate
+   	}
+}
