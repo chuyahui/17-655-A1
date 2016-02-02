926c030eb5c5e7a85b46ac7b301c674b8e50dc43
Weinan Qiu <davidiamyou@gmail.com>
2016-02-01 22:05:04 -0500

    pressure formatting filter comment.

--- A1/common/src/main/java/shared/PressureFormattingFilter.java
+++ A1/common/src/main/java/shared/PressureFormattingFilter.java
@@ -8,25 +8,40 @@ import java.text.DecimalFormat;
 import java.util.Arrays;
 
 /**
- * @author Weinan Qiu
+ * A filter that converts the pressure data to a string format.
+ *
  * @since 1.0.0
  */
 public class PressureFormattingFilter extends SimpleFilter {
 
+    /**
+     * The format of the pressure data.
+     */
     private String numberFormat = "#00.00000";
 
     public PressureFormattingFilter(String filterId, MeasurementConfig context) {
         super(context, filterId);
     }
 
+    /**
+     * Format the pressure data and write the padded byte array out.
+     *
+     * @param id the id of the measurement data
+     * @param measurement data
+     *
+     * @return the padded byte array of formatted pressure data or any other data as is.
+     */
     @Override
     protected byte[] doTransform(int id, byte[] measurement) {
+        // return the measurement as is if it is not pressure
         if (id != MeasurementConfig.ID_PRESSURE)
             return measurement;
 
+        // do format
         double pressure = ConversionUtil.convertToDouble(measurement);
         String formatted = new DecimalFormat(numberFormat).format(pressure);
 
+        // pad the byte array and return the formatted data.
         return Arrays.copyOf(formatted.getBytes(), 9);
     }
 }
