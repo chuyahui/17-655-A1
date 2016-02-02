25b0c5691dc03942773a749104877dd746f5181e
Weinan Qiu <davidiamyou@gmail.com>
2016-02-01 23:40:01 -0500

    altitude filter comment

--- A1/systemC/src/main/java/system/AltitudeFilter.java
+++ A1/systemC/src/main/java/system/AltitudeFilter.java
@@ -8,26 +8,43 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
 /**
- * @author Weinan Qiu
+ * A filter that routes the entire data frame whose altitude is below a certain threshold to the upper output port and
+ * others to the lower output port.
+ *
  * @since 1.0.0
  */
 public class AltitudeFilter extends SplittingFilterTemplate {
 
+    /**
+     * Threshold used to identity whether data frame should be routed to upper or lower ports
+     */
     private double threshold = 10000d;
 
+    /**
+     * Cache for the current data frame
+     */
     private DataFrame currentFrame;
 
     public AltitudeFilter(String filterId, MeasurementConfig context) {
         super(filterId, context);
     }
 
+    /**
+     * Collects data for the data frame and routes the frame.
+     *
+     * @param idBytes
+     * @param measurement measurement data bytes
+     */
     @Override
     protected void routeMeasurement(byte[] idBytes, byte[] measurement) {
         int id = ConversionUtil.convertToInt(idBytes);
 
+        // create frame if not exists
         if (currentFrame == null)
             currentFrame = new DataFrame();
 
+        // cache frame data (collect only time, pressure and altitude and assume at this stage, all other measurements
+        // have been dropped)
         if (id == MeasurementConfig.ID_TIME)
             currentFrame.time = measurement;
         else if (id == MeasurementConfig.ID_PRESSURE)
@@ -37,27 +54,34 @@ public class AltitudeFilter extends SplittingFilterTemplate {
         else
             throw new RuntimeException("measurement with id " + id + " should have been discarded already!");
 
+        // if we haven't collected everything, return for now
         if (!currentFrame.hasCollectedAll())
             return;
 
+        // from this point on, we have the current data frame containing time, pressure and altitude.
+
         try {
+            // if less than threshold (by default, 10K), route to upper port
             if (currentFrame.altitudeInFeet() < threshold) {
                 for (byte eachByte : currentFrame.getAllBytes())
                     WriteFilterOutputPortOne(eachByte);
-            } else {
+            }
+            // otherwise, route to lower port
+            else {
                 for (byte eachByte : currentFrame.getAllBytes())
                     WriteFilterOutputPortTwo(eachByte);
             }
         } catch (IOException ex) {
             throw new RuntimeException("Encountered error while routing measurement: " + ex.getMessage());
         } finally {
+            // reset the current frame after routing to prepare for the next frame
             currentFrame = null;
         }
     }
 
     @Override
     protected void reachedEndOfStream() {
-
+        // no need to do anything on end of stream since we don't cache more than 1 frame
     }
 
     public double getThreshold() {
@@ -68,6 +92,9 @@ public class AltitudeFilter extends SplittingFilterTemplate {
         this.threshold = threshold;
     }
 
+    /**
+     * Structure to hold time, altitude and pressure data, represents a data frame
+     */
     class DataFrame {
         public byte[] time;
         public byte[] altitude;
@@ -83,6 +110,11 @@ public class AltitudeFilter extends SplittingFilterTemplate {
                     pressure != null;
         }
 
+        /**
+         * Utility method to align all the bytes for time, altitude and pressure data (with id)
+         * @return
+         * @throws IOException
+         */
         public byte[] getAllBytes() throws IOException {
             ByteArrayOutputStream out = new ByteArrayOutputStream();
             out.write(ConversionUtil.convertToByteArray(MeasurementConfig.ID_TIME));
