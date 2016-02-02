6da47e108a61199369aa304492f2aef8fd1e16cb
Weinan Qiu <davidiamyou@gmail.com>
2016-02-01 23:43:03 -0500

    altitude formatting filter comment

--- A1/systemC/src/main/java/system/AltitudeFormattingFilter.java
+++ A1/systemC/src/main/java/system/AltitudeFormattingFilter.java
@@ -8,25 +8,41 @@ import java.text.DecimalFormat;
 import java.util.Arrays;
 
 /**
- * @author Weinan Qiu
+ * Simple filter to format the altitude data into string.
+ *
  * @since 1.0.0
  */
 public class AltitudeFormattingFilter extends SimpleFilter {
 
+    /**
+     * String format of the altitude measurement
+     */
     private String numberFormat = "#000000.00000";
 
     public AltitudeFormattingFilter(String filterId, MeasurementConfig context) {
         super(context, filterId);
     }
 
+    /**
+     * Read altitude data and perform string format.
+     *
+     * @param id the id of the measurement data
+     * @param measurement data
+     * @return
+     */
     @Override
     protected byte[] doTransform(int id, byte[] measurement) {
+        // return measurement as is if it is not altitude
         if (id != MeasurementConfig.ID_ALTITUDE)
             return measurement;
 
+        // read measurement to double
         double altitudeInFeet = ConversionUtil.convertToDouble(measurement);
+
+        // format to string
         String formatted = new DecimalFormat(numberFormat).format(altitudeInFeet);
 
+        // return padded bytes
         return Arrays.copyOf(formatted.getBytes(), 13);
     }
 
