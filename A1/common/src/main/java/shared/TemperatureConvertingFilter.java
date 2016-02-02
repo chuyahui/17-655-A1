241dd6cd5a7053334b73d34647ff37bc43247bc3
Weinan Qiu <davidiamyou@gmail.com>
2016-02-01 22:13:32 -0500

    temperature converting filter comment.

--- A1/common/src/main/java/shared/TemperatureConvertingFilter.java
+++ A1/common/src/main/java/shared/TemperatureConvertingFilter.java
@@ -8,28 +8,45 @@ import java.text.DecimalFormat;
 import java.util.Arrays;
 
 /**
- * @author Weinan Qiu
+ * The filter that converts temperature data from fahrenheit to celsius and formats it to string.
+ *
  * @since 1.0.0
  */
 public class TemperatureConvertingFilter extends SimpleFilter {
 
+    /**
+     * String format of the temperature data in celsius
+     */
     private String numberFormat = "#000.00000";
 
     public TemperatureConvertingFilter(String filterId, MeasurementConfig context) {
         super(context, filterId);
     }
 
+    /**
+     * Do conversion for temperature data and format it into string.
+     *
+     * @param id the id of the measurement data
+     * @param measurement data
+     * @return
+     */
     @Override
     protected byte[] doTransform(int id, byte[] measurement) {
+        // return data as is if it is not temperature
         if (id != MeasurementConfig.ID_TEMPERATURE) {
             return measurement;
         }
 
-
+        // read data in fahrenheit
         double tempInFahrenheit = ConversionUtil.convertToDouble(measurement);
+
+        // convert data to celsius
         double tempInCelsius = (tempInFahrenheit - 32d) / 1.8d;
+
+        // format data
         String formatted = new DecimalFormat(numberFormat).format(tempInCelsius);
 
+        // return padded data
         return Arrays.copyOf(formatted.getBytes(), 10);
     }
 
